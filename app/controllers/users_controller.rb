f156934d37375797006dcfb7e22bdfb2077938f8
yuekami <yuekami7@gmail.com>
2023-12-16 18:35:05 +0900

    ユーザー登録機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:name],email: params[:email])
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new", status: :unprocessable_entity)
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/index'
   get "users/:id", to:"users#show"
+  get "signup", to:"users#new"
+  post "users/create"
 
   get 'posts/index'
   get "posts/new"
