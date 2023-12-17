394ae890f877472b10d670d36c3281d857faaffc
yuekami <yuekami7@gmail.com>
2023-12-17 21:27:52 +0900

    自分の投稿のみ編集・削除できるように対応

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,21 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :ensure_correct_user,{only: [:edit, :update, :destroy]}
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @current_user.id != @post.user_id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
 
   def index
     @posts = Post.all.order(created_at: :desc)
   end
   def show
     @post = Post.find_by(id:params[:id])
-    @user = User.find_by(id: @post.user_id)
+    @user = @post.user
   end
 
   def new
